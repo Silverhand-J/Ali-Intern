323596395fd6184b77ca6cc7575700a211e024a0
sheny <1250873589@qq.com>
2026-01-29 01:16:45 +0800

    Initialize project

---
+++ src/main/java/com/example/aliintern/AliInternApplication.java
@@ -0,0 +1,13 @@
+package com.example.aliintern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliInternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliInternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aliintern/AliInternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aliintern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliInternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
